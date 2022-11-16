aada616b46450e3a2b7b5b259ea9fab7cbdce652
Tim South <timmysouth7@gmail.com>
2022-11-16 03:30:01 -0500

    Initial Commit

---
+++ StarWars2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWars2
+//
+//  Created by Tim South on 11/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
